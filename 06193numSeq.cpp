8fb306e5b2076bb6b3a43bfdf9518c2656064d2d
Joowon <joowon7508@gmail.com>
2018-11-23 19:19:49 +0900

    I don't know what to do...

--- 06193numSeq.cpp
+++ 06193numSeq.cpp
@@ -3,30 +3,6 @@
 
 using namespace std;
 
-// change if possible to satisfy two places
-bool canTwoPlace(vector<int> &front, vector<int> &back, int sameI) {
-  for(int i = sameI; i < front.size(); i++) {
-    for(int j = i+1; j < front.size(); j++) {
-      // case 1) same for two in sequence
-      // 1 1 2 2
-      if (front[i] == front[j] && back[i] == back[j]) {
-	front[j] = back[j];
-	back[i] = front[i];
-	return true;
-      }
-
-      // case 2) change within back part
-      // 1 2 2 1
-      if (front[i] == back[j] && front[j] == back[i]) {
-	back[i] = front[i];
-	back[j] = front[j];
-	return true;
-      }
-    }
-  }
-  return false;
-}
-
 int main() {
   int Num;
   cin >> Num;
@@ -42,60 +18,46 @@ int main() {
     for(int i = 0; i < length; i++)
       cin >> back[i];
 
-    int cnt = 0, sameIndex = 0;
-    while(cnt < 10) {
-      // stop when the two is the same
-      while(front[sameIndex] == back[sameIndex] && sameIndex < length) sameIndex++;
-      
-      if (sameIndex >= length) {
-	cout << "#" << n << " " << cnt << endl;
-	break;
-      }
+    // find bounds
+    int sameNum = 0;
+    for(int i = 0; i < length; i++)
+      if (front[i] == back[i]) sameNum++;
+    length = length - sameNum;
+    
+    int upper, lower;
+    upper = length - 1;
+    lower = length / 2;
+    if (length % 2 == 1) lower++;
 
-      // check if a change can satisfy two places
-      if (!canTwoPlace(front, back, sameIndex)) {
-	// change that satisfy one place
+    
+    // cout << lower << " ~ " << upper << endl;
 
-	// seq 1) find first not-match index
-	int x = sameIndex;
-	
-	// seq 2) find index of first[x] - at front vector
-	bool ifChanged = false;
-	for(int y = sameIndex+1; y < length; y++) {
-	  if (front[x] == front[y]){
-	    // swap
-	    front[y] = back[x];
-	    back[x] = front[x];
-	    ifChanged = true;
-	    break;
-	  }
-	}
-
-	if(!ifChanged) {
-	  // seq 2) find index of first[x] - at back vector
-	  for(int y = sameIndex+1; y < length; y++) {
-	    if (front[x] == back[y]) {
-	      // swap
-	      back[y] = back[x];
-	      back[x] = front[x];
-	      break;
-	    }
-	  }
-	}
-      }
-
-      cnt++;
-
-
-      // Print for test
-      // cout << cnt << " : ";
-      // for(int i = 0; i < length; i++)
-      // 	cout << front[i] << " ";
-      // cout << "\t";
-      // for(int i = 0; i < length; i++)
-      // 	cout << back[i] << " ";
-      // cout << endl;
+    // end case
+    if (lower == upper) {
+      cout << "#" << n << " " << upper << endl;
+      continue;
     }
+
+    // decrease when a change satisfy two places 
+    for (int i = 0; i < front.size(); i++) {
+      for (int j = i+1; j < front.size(); j++) {
+	// case 1) same for two in sequence - 1 1 2 2
+	if (front[i] == front[j] && back[i] == back[j]) {
+	  upper--;
+	  break;
+	}
+
+	// case 2) change within back part - 1 2 2 1
+	if (front[i] == back[j] && front[j] == back[i]) {
+	  upper--;
+	  break;
+	}
+      }
+    }
+
+    // out
+    if (upper <= lower) cout << "#" << n << " " << lower << endl;
+    else cout << "#" << n << " " << upper << endl;
   }
   
   return 0;
