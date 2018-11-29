c2ec743989134df7fc207655ecb5572a904e7163
Joowon <joowon7508@gmail.com>
2018-11-29 09:11:44 +0900

    dirty prob

--- 01798travel.cpp
+++ 01798travel.cpp
@@ -1,45 +1,62 @@
 #include <iostream>
-#define timelimit 540;
+#define timelimit 540
 
 using namespace std;
 
 int nodeNum, travelDay;
 int travelCost[40][40], happy[40], visited[40];
-int airportNum, hotelNum[40];
+int airportNum;
 int maxHappy=0, bestRoute[40], cnt = 0;
 
-void dfs(int nodeNow, int timespent, int nthDay, int acqHappy, int route[40], int nthVisit) {
+int tourSpot[40], tourNum;
+int hotelSpot[40], hotelNum;
+
+int dfs(int nodeNow, int timespent, int nthDay, int acqHappy, int route[40], int nthVisit) {
   //  cout << "DAY" << nthDay << " Node " << nodeNow << " acqHappy: " << acqHappy << " time : " << timespent << endl;
-  if (timespent > 540) return;
-  else if (nodeNow == airportNum && nthDay == travelDay && acqHappy != 0) {
-    // check if best
-    if (acqHappy > maxHappy) {
-      maxHappy = acqHappy;
-      for(int i = 0; i < nthVisit; i++)
-	bestRoute[i] = route[i];
-      cnt = nthVisit;
 
-      // print for test
-      // cout << "DAY" << nthDay << " Node " << nodeNow << " acqHappy: " << acqHappy << " time : " << timespent << endl;
-      // for(int i = 0; i < nthVisit; i++)
-      // 	cout << route[i]+1 << " ";
-      // cout << endl;
-    }
-    return;
-  }
-  else if (hotelNum[nodeNow]) {
-    timespent = 0; nthDay++;
-    if (nthDay > travelDay) return;
-    visited[nodeNow] = false;
-  }
-
-  // int maxHappy = 0;
-  for(int i = 0; i < nodeNum; i++) {
-    if (visited[i]) continue;
+  int visitAny = false;
+  // visit tour spot if possible
+  for(int t = 0; t < tourNum; t++) {
+    int i = tourSpot[t];
+    if (visited[i] || timespent+travelCost[nodeNow][i]>timelimit) continue;
     visited[i] = true; route[nthVisit] = i;
-    dfs( i, timespent+travelCost[nodeNow][i], nthDay, acqHappy+happy[i], route, nthVisit+1);
+    int sucess = dfs( i, timespent+travelCost[nodeNow][i], nthDay, acqHappy+happy[i], route, nthVisit+1);
+    if (sucess != -1) visitAny = true;
     visited[i] = false;
   }
+  // visit hotel/airport only if there is no time for tour
+  if(!visitAny) {
+    if (nthDay == travelDay) { // go to airport
+      if (timespent+travelCost[nodeNow][airportNum]<=timelimit) {
+	route[nthVisit] = airportNum;
+	// check if best
+	if (acqHappy > maxHappy) {
+	  maxHappy = acqHappy;
+	  for(int i = 0; i <= nthVisit; i++)
+	    bestRoute[i] = route[i];
+	  cnt = nthVisit+1;
+
+	  // print for test
+	//   cout << "DAY" << nthDay << " Node " << nodeNow << " acqHappy: " << acqHappy << " time : " << timespent << endl;
+	//   for(int i = 0; i <= nthVisit; i++)
+	//     cout << route[i]+1 << " ";
+	//   cout << endl;
+	}
+	visitAny = true;
+      }
+    }
+    else { // go to hotel
+      for(int h = 0; h < hotelNum; h++) {
+	int i = hotelSpot[h];
+	if (timespent+travelCost[nodeNow][i]>timelimit) continue;
+	route[nthVisit] = i;
+	int sucess = dfs( i, 0, nthDay+1, acqHappy, route, nthVisit+1);
+	if (sucess != -1) visitAny = true;
+      }
+    }
+  }
+  if (!visitAny) return -1;
+  else return 1;
 }
 
 int main() {
@@ -47,11 +64,11 @@ int main() {
   cin >> caseNum;
 
   for(int n = 1; n <= caseNum; n++) {
-    maxHappy = 0;
+    maxHappy = tourNum = hotelNum = 0;
     cin >> nodeNum >> travelDay;
 
     for(int i = 0; i < nodeNum; i++) {
-      hotelNum[i] = 0; visited[i] = 0;
+      visited[i] = 0;
       for(int j = i+1; j < nodeNum; j++) {
 	cin >> travelCost[i][j];
 	travelCost[j][i] = travelCost[i][j];
@@ -64,12 +81,13 @@ int main() {
       if (temp == 'A') airportNum = i;
       else if (temp== 'P') {
 	cin >> temp2 >> happy[i];
+	tourSpot[tourNum++] = i;
 	// consider spending time as moving cost
 	for (int j = 0; j < nodeNum; j++)
 	  travelCost[j][i] += temp2;
       }
       else {
-	hotelNum[i] = 1;
+	hotelSpot[hotelNum++] = i;
 	happy[i] = 0;
       }
     }
